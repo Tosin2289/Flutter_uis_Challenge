2016f008345c5afc839c43bcf4779e9db67de4d1
Tosin2289 <ayoolatosin00@gmail.com>
2023-04-26 20:55:14 +0100

    second commit

---
+++ lib/days/day115.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+
+class day115 extends StatefulWidget {
+  static const String path = "lib/src/pages/profile/profile12.dart";
+
+  const day115({super.key});
+
+  @override
+  _day115State createState() => _day115State();
+}
+
+class _day115State extends State<day115> {
+  final bool _isOpen = false;
+  var imageList = [
+    'images/assets115/img1.jpg',
+    'images/assets115/img2.jpg',
+    'images/assets115/img3.webp',
+    'images/assets115/img4.jpg',
+    'images/assets115/img5.jpg',
+    'images/assets115/img6.jpg',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          FractionallySizedBox(
+            alignment: Alignment.topCenter,
+            heightFactor: 0.7,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/assets115/img7.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          FractionallySizedBox(
+            alignment: Alignment.bottomCenter,
+            heightFactor: 0.5,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(50))),
+              child: panelBody(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SingleChildScrollView panelBody() {
+    double hPadding = 40;
+    return SingleChildScrollView(
+      physics: const ClampingScrollPhysics(),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: hPadding),
+            height: MediaQuery.of(context).size.height * 0.35,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _titleSection(),
+                _infoSection(),
+                _actionSection(hPadding: hPadding),
+              ],
+            ),
+          ),
+          GridView.builder(
+            primary: false,
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            itemCount: imageList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 16,
+            ),
+            itemBuilder: (BuildContext context, int index) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(imageList[index]),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Row _actionSection({double? hPadding}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Visibility(
+          visible: !_isOpen,
+          child: Expanded(
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(color: Colors.blue),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'VIEW PROFILE',
+                style: TextStyle(
+                  fontFamily: 'NimbusSanL',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Visibility(
+          visible: !_isOpen,
+          child: const SizedBox(
+            width: 16,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: _isOpen
+                  ? (MediaQuery.of(context).size.width - (2 * hPadding!)) / 1.6
+                  : double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                ),
+                onPressed: () => print('Message tapped'),
+                child: const Text(
+                  'MESSAGE',
+                  style: TextStyle(
+                    fontFamily: 'NimbusSanL',
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row _infoSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        _infoCell(title: 'Projects', value: '1135'),
+        Container(
+          width: 1,
+          height: 40,
+          color: Colors.grey,
+        ),
+        _infoCell(title: 'Hourly Rate', value: "\$65"),
+        Container(
+          width: 1,
+          height: 40,
+          color: Colors.grey,
+        ),
+        _infoCell(title: 'Location', value: 'Dusseldorf'),
+      ],
+    );
+  }
+
+  Column _infoCell({required String title, required String value}) {
+    return Column(
+      children: <Widget>[
+        Text(
+          title,
+          style: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.w300,
+            fontSize: 14,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          value,
+          style: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.w700,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column _titleSection() {
+    return Column(
+      children: const <Widget>[
+        Text(
+          'Aanika Johnson',
+          style: TextStyle(
+            fontFamily: 'NimbusSanL',
+            fontWeight: FontWeight.w700,
+            fontSize: 30,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          'Freelancer',
+          style: TextStyle(
+            fontFamily: 'NimbusSanL',
+            fontStyle: FontStyle.italic,
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
