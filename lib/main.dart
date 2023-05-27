7f15e14e430fd5dbd6db5910d12ad21bc10f7dd0
Tosin2289 <ayoolatosin00@gmail.com>
2023-05-27 14:11:57 +0100

    seventy commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-import 'days/day142.dart';
+import 'days/day143.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: const day142(),
+      home: const day143(),
     );
   }
 }
