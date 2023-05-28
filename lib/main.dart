41724d38206db468308333c87af1560de8c27cfa
Tosin2289 <ayoolatosin00@gmail.com>
2023-05-28 20:40:12 +0100

    seventy first

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'days/day143.dart';
+import 'days/day144.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: const day143(),
+      home: const day144(),
     );
   }
 }
