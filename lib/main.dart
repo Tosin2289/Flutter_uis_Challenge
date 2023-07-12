55ea8bc8d86463f794b84b0ea9667ed3a5930903
Tosin2289 <ayoolatosin00@gmail.com>
2023-07-12 21:39:46 +0100

    day194

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'days/day193.dart';
+import 'days/day194.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: const Day193(),
+      home: const Day194(),
     );
   }
 }
