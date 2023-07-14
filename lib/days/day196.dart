70284a19a5cd8940f3a87b70aa41d3a439067bb3
Tosin2289 <ayoolatosin00@gmail.com>
2023-07-14 22:14:44 +0100

    day196

--- lib/days/day196.dart
+++ lib/days/day196.dart
@@ -79,10 +79,157 @@ class Day196 extends StatelessWidget {
                             color: Colors.white.withOpacity(.5),
                             fontWeight: FontWeight.normal),
                       ),
+                      const SizedBox(
+                        height: 10,
+                      ),
                     ],
-                  )
+                  ),
                 ],
               )),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: const [
+                Text(
+                  "Summary",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Fcon(
+                    concolor: Colors.red.shade100,
+                    icon: Icons.battery_charging_full_rounded,
+                    score: '80',
+                    text: 'Reaction',
+                    textcolor: Colors.red),
+                Fcon(
+                    concolor: Colors.yellow.shade100,
+                    icon: Icons.memory,
+                    score: '92',
+                    text: 'Memory',
+                    textcolor: Colors.yellow),
+                Fcon(
+                    concolor: Colors.green.shade100,
+                    icon: Icons.chat_bubble_outline_outlined,
+                    score: '61',
+                    text: 'Verbal',
+                    textcolor: Colors.green),
+                Fcon(
+                    concolor: Colors.deepPurple.shade100,
+                    icon: Icons.battery_charging_full_rounded,
+                    score: '72',
+                    text: 'Visual',
+                    textcolor: Colors.deepPurple),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.shade900,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: const Center(
+                child: Text(
+                  "Continue",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Fcon extends StatelessWidget {
+  final icon;
+  final String text;
+  final textcolor;
+  final score;
+  final concolor;
+  const Fcon(
+      {super.key,
+      required this.concolor,
+      required this.icon,
+      required this.score,
+      required this.text,
+      required this.textcolor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          Container(
+            height: 65,
+            width: MediaQuery.of(context).size.width * .9,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: concolor),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        icon,
+                        color: textcolor,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        text,
+                        style: TextStyle(
+                            color: textcolor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          score,
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          " / 100",
+                          style: TextStyle(
+                              color: Colors.black.withOpacity(.3),
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
